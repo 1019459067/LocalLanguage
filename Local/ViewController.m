4cee3c7d0c5abfe359d898114063dc40b7a24dff
XiaoWeiHua <xiaoweihua@sensetime.com>
2015-08-24 10:37:29 +0800

    完成字符串本地化。

--- Local/ViewController.m
+++ Local/ViewController.m
@@ -17,7 +17,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    //add label
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height * 0.1)];
+    label.center = CGPointMake(self.view.frame.size.width * 0.5, self.view.frame.size.height * 0.73);
+    label.backgroundColor = [UIColor orangeColor];
+    label.numberOfLines = 0;
+    label.text = @"如果你想要成功,不要去追求成功;尽管做你自己热爱的事情并且相信它,成功自然到来.";
+    label.text = NSLocalizedString(@"如果你想要成功,不要去追求成功;尽管做你自己热爱的事情并且相信它,成功自然到来.", nil);
+//    label.text = NSLocalizedStringFromTable(@"start", @"Chinese", @"sd");
+    [self.view addSubview:label];
 }
 
 - (void)didReceiveMemoryWarning {
